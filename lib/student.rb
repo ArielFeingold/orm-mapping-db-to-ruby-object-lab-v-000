d3729514cf536b5eeabb2f9fb19bf0f822a0877e
Ariel Feingold <feingold.ariel@gmail.com>
2018-03-20 18:42:18 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,8 @@ class Student
   def self.new_from_db(row)
     new_student = self.new
     new_student.id = row[0]
-    new_student.name = [1]
-    new_student.grade = [2]
+    new_student.name = row[1]
+    new_student.grade = row[0]
     new_student
     binding.pry
   end
